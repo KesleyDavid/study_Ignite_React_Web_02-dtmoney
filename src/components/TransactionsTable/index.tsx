43872cdf74eace3e8d747b51d0fc36d9ddcaa226
Kesley David <kesleygti@gmail.com>
2021-03-15 18:21:15 -0300

    [02] [02] [project] Component: TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import * as S from './styles';
+
+export function TransactionsTable() {
+  return (
+    <S.Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Sistema</td>
+            <td className="deposit">R$ 12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/21</td>
+          </tr>
+          <tr>
+            <td>Pagamento de imposto</td>
+            <td className="withdraw">- R$ 500</td>
+            <td>Tributos</td>
+            <td>20/02/21</td>
+          </tr>
+        </tbody>
+      </table>
+    </S.Container>
+  );
+}
